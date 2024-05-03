45ffac13e641706a4622033717a63f2ee5849600
Beto Villalba <betovill26@gmail.com>
2024-05-03 14:30:03 -0400

    fix index clases

--- resources/js/Pages/Clases/Index.vue
+++ resources/js/Pages/Clases/Index.vue
@@ -36,11 +36,11 @@ const fetchClases = async () => {
                 (item) => item.docente_id == data.id
             );
         } else if (data.role == "alumno") {
-            showClases.value = props.clases.filter((item) => {
-                if (item.alumnos.length !== 0) {
-                    return item.alumnos[0].id == data.id;
-                }
-            });
+  
+            showClases.value = props.clases.filter((clase) =>
+                clase.alumnos.some((alumno) => alumno.id == data.id)
+            );
+
         } else if (data.role == "administrador") {
             showClases.value = props.clases;
         }
@@ -58,7 +58,6 @@ const deleteClase = (id) => {
 };
 
 onMounted(fetchClases);
-
 </script>
 
 <template>
@@ -66,10 +65,14 @@ onMounted(fetchClases);
         <Head title="Clases" />
 
         <template #header>
-            <h2 class="font-semibold text-xl text-primary flex items-center gap-4">
-                <div class="w-7 h-7 flex items-center justify-center rounded-full bg-primary border shadow-md">
+            <h2
+                class="font-semibold text-xl text-primary flex items-center gap-4"
+            >
+                <div
+                    class="w-7 h-7 flex items-center justify-center rounded-full bg-primary border shadow-md"
+                >
                     <Icon name="clases" class="w-3 h-3 fill-white" />
-                </div>           
+                </div>
                 Todas las Clases
             </h2>
         </template>
@@ -77,7 +80,8 @@ onMounted(fetchClases);
             <!-- create button -->
             <div
                 class="flex items-center mb-6"
-                v-if="$page.props.userRole.role.rol === 'docente'">
+                v-if="$page.props.userRole.role.rol === 'docente'"
+            >
                 <Link class="btn-indigo" href="/clases/create">
                     <span>Crear</span>
                     <span class="hidden md:inline">&nbsp;Clase</span>
@@ -85,32 +89,42 @@ onMounted(fetchClases);
             </div>
             <div class="w-full overflow-x-auto">
                 <div
-                    class="py-4 px-4 flex flex-wrap items-center justify-center md:justify-start gap-6">
+                    class="py-4 px-4 flex flex-wrap items-center justify-center md:justify-start gap-6"
+                >
                     <!-- cards -->
                     <template v-for="clase in showClases" :key="clase.id">
                         <card>
                             <template #cardHeader>
                                 <Link
                                     class="inline-block pb-4 w-full group"
-                                    :href="route('clases.show', clase.id)">
+                                    :href="route('clases.show', clase.id)"
+                                >
                                     <span
-                                        class="block text-3xl text-white font-bold capitalize group-hover:underline">
+                                        class="block text-3xl text-white font-bold capitalize group-hover:underline"
+                                    >
                                         {{ clase.materia.nombre }}
                                     </span>
                                     <span
-                                        class="block text-md font-md text-white">
+                                        class="block text-md font-md text-white"
+                                    >
                                         {{ clase.codigo }}
                                     </span>
                                 </Link>
                                 <Dropdown
                                     class="self-start"
-                                    v-if="$page.props.userRole.role.rol !== 'alumno'">
+                                    v-if="
+                                        $page.props.userRole.role.rol !==
+                                        'alumno'
+                                    "
+                                >
                                     <template #trigger>
                                         <div
-                                            class="pt-2 pr-1 pb-1 overflow-hidden flex items-center justify-end">
+                                            class="pt-2 pr-1 pb-1 overflow-hidden flex items-center justify-end"
+                                        >
                                             <button
                                                 :open="open"
-                                                class="flex h-6 w-6 pt-1 rounded-full justify-center overflow-hidden z-10 group hover:ring-1 hover:ring-white focus:ring-1 focus:ring-white">
+                                                class="flex h-6 w-6 pt-1 rounded-full justify-center overflow-hidden z-10 group hover:ring-1 hover:ring-white focus:ring-1 focus:ring-white"
+                                            >
                                                 <icon
                                                     name="trigger"
                                                     class="w-[0.2rem] h-[0.2rem] fill-white"
@@ -122,14 +136,24 @@ onMounted(fetchClases);
                                         <div class="p-3">
                                             <Link
                                                 class="inline-block py-2 font-bold w-full h-full text-primary hover:underline"
-                                                :href="route('clases.edit',clase.id)"
-                                                v-if="$page.props.userRole.role.rol === 'docente'">
+                                                :href="
+                                                    route(
+                                                        'clases.edit',
+                                                        clase.id
+                                                    )
+                                                "
+                                                v-if="
+                                                    $page.props.userRole.role
+                                                        .rol === 'docente'
+                                                "
+                                            >
                                                 Editar
                                             </Link>
                                             <button
                                                 class="inline-block py-2 text-left font-bold w-full h-full text-primary hover:underline"
                                                 type="button"
-                                                @click="deleteClase(clase.id)">
+                                                @click="deleteClase(clase.id)"
+                                            >
                                                 Eliminar
                                             </button>
                                         </div>
@@ -138,9 +162,11 @@ onMounted(fetchClases);
                             </template>
                             <template #cardFooter>
                                 <div
-                                    class="h-full flex px-4 items-center justify-between">
+                                    class="h-full flex px-4 items-center justify-between"
+                                >
                                     <span
-                                        class="text-xs font-bold text-primary italic capitalize">
+                                        class="text-xs font-bold text-primary italic capitalize"
+                                    >
                                         {{ clase.estado.estado }}
                                     </span>
                                     <span class="text-xs text-gray-400 italic">
